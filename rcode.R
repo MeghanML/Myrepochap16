6ac6d510dc8362709c25ec4d8f1960f02b537086
meghan <meghan@datascience.co.il>
2017-10-17 10:49:10 +0300

    new delay varaible

--- rcode.R
+++ rcode.R
@@ -1,6 +1,9 @@
 library(tidyverse)
+library(nycflights13)
+?flights
 
-mpg
 
 dec25 <- filter(flights, month==12, day==25)
 nov_dec <- filter (flights, month %in% c(11, 12))
+janv1 <- filter(flights, month ==1, day ==1)
+delay_more_120 <- filter(flights, dep_delay > 120 | arr_delay > 120 )
